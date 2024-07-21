6be086f77cb7f8f2a74c51a05a81d94968666369
Ravi Raj Singh <134828819+raviraj0922@users.noreply.github.com>
2024-07-21 23:44:36 +0530

    update the file

--- day2.js
+++ day2.js
@@ -1,6 +1,52 @@
 // Add two number and log the result
-let a = 10;
+let a = 30;
 let b = 20;
 const sum = a+b;
 console.log("sum:", sum)
 
+// Subtract two number and log the result
+const sub = a-b;
+console.log("sub:", sub);
+
+// Multiply two number and log the result
+const multi = a*b;
+console.log("Multiply:", multi);
+
+// Divide two number and log the result
+const divide = a/b;
+console.log("Divide:", divide);
+
+// find the reminder when one number is divided by another and log the result
+const reminder = a % b;
+console.log("Reminder:", reminder);
+
+// use += operator to add a number to variable and log teh result
+let x = 3; //initial total is x
+let y = 5; 
+x += y; // y added to total means x+y
+console.log("Update Total:", x);
+
+// use -= operator to subtract a number to variable and log teh result
+let c = 3; //initial total is c
+let d = 5; 
+c -= d;
+console.log("Update Total:", c);
+
+// to compare two number using > < and log the result
+const num1 = 10;
+const num2 = 5;
+console.log(num1 > num2);
+
+const num3 = 7;
+const num4 = 12;
+console.log(num3 < num4);
+
+// to compare two number using >= <= and log the result
+console.log(num1 >= num2);
+
+console.log(num3 <= num4);
+
+// to compare two number using == === and log the result
+console.log(num1 == num2); //Equality
+
+console.log(num3 === num4); //Strict Equality
